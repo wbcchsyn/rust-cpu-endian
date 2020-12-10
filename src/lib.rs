be480cd4516f8ed3758800f32a221dc2fcccff28
Yoshida Shin <wbcchsyn@gmail.com>
2020-12-10 20:24:07 +0900

    Crate document

--- src/lib.rs
+++ src/lib.rs
@@ -69,7 +69,36 @@
 
 #![deny(missing_docs)]
 
-//! # cpu_endian
+//! # cpu-endian
+//!
+//! `cpu-endian` is a portable crate to detect CPU byte order.
+//!
+//! ## Feature
+//!
+//! - Enable to detect scalar is ordered in little-endian, big-endian, and the other minor endians (like PDP-endian, mixed-endian, middle-endian, and so on.)
+//! - Checks dynamically when first asked and cache the result. Some CPUs switch the endian, however, it will never be changed while the process is running.
+//!
+//! ## Examples
+//!
+//! ```
+//! use cpu_endian::{Endian, working};
+//!
+//! // Takes first octet of 0x00ff: u16.
+//! let v: u16 = 0x00ff;
+//! let first_octet: u8 = unsafe {
+//!     let ptr = &v as *const u16;
+//!     let ptr = ptr as *const u8;
+//!     *ptr
+//! };
+//!
+//! // If the byte-order is little-endian, the first octet should be 0xff, or if big-endian,
+//! // it should be 0x00.
+//! match working() {
+//!     Endian::Little => assert_eq!(0xff, first_octet),
+//!     Endian::Big => assert_eq!(0x00, first_octet),
+//!     _ => {},
+//! }
+//! ```
 
 use core::sync::atomic::{AtomicU8, Ordering};
 use std::os::raw::c_int;
@@ -93,7 +122,7 @@ static NATIVE: AtomicU8 = AtomicU8::new(0);
 
 /// Returns the CPU byte order.
 ///
-/// Some CPUs change the byte order, however, it won't be changed while process is running.
+/// Some CPUs switch the byte order, however, it won't be changed while process is running.
 ///
 /// This function checks the byte order dynamically and caches it when first called.
 /// After that, this function returns the cache.
@@ -109,7 +138,7 @@ static NATIVE: AtomicU8 = AtomicU8::new(0);
 ///     *ptr
 /// };
 ///
-/// // If the byte-order is little-endian, the first octet should be 0xff, or if big-endian, the
+/// // If the byte-order is little-endian, the first octet should be 0xff, or if big-endian,
 /// // it should be 0x00.
 /// match working() {
 ///     Endian::Little => assert_eq!(0xff, first_octet),
